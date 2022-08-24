8dbaf7088cf7baa8589082fdf49215e28d1fa39e
Cemil <chrisw@cydeo.com>
2022-08-24 17:52:23 +0300

    meeting practice tasks modified

--- src/main/java/lambda_sort_doublecolon_tasks/Employee.java
+++ src/main/java/lambda_sort_doublecolon_tasks/Employee.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 public class Employee {
 
--- src/main/java/lambda_sort_doublecolon_tasks/MeetingPractice.java
+++ src/main/java/lambda_sort_doublecolon_tasks/MeetingPractice.java
@@ -1,13 +1,8 @@
 package lambda_sort_doublecolon_tasks;
 
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static lambda_sort_doublecolon_tasks.DataGenerator.*;
-import static lambda_sort_doublecolon_tasks.Service.*;
+import static lambda_sort_doublecolon_tasks.Service.getAllEmployees;
 
 public class MeetingPractice {
 
@@ -18,17 +13,8 @@ public class MeetingPractice {
 
         //When you start to solve new question, comment out the System.out.println statement
 
-
-        //EXAMPLE
- //       System.out.println("*** get all employees if their Id number is odd");
-//        List<Employee> list0 = filter(getAllEmployees(), employee -> employee.getId() % 2 != 0);
-//        list0.forEach(System.out::println);
-
-//       getAllEmployees().forEach(employee -> System.out.println(employee.getId() % 2 == 0));
-//
-//        getAllEmployees().stream()
-//                .filter(employee -> employee.getId() % 2 != 0)
-//                .forEach(System.out::println);
+        // EXAMPLE
+        // System.out.println("*** get all employees if their Id number is odd");
 
 
         //System.out.println("*** get all employees if their Id number is even");
@@ -53,38 +39,34 @@ public class MeetingPractice {
 
 
         //System.out.println("***print all employees***");
-    //    printAll(getAllEmployees(), System.out::println);
+        //    printAll(getAllEmployees(), System.out::println);
 
 
         //System.out.println("***SORTING QUESTIONS***");
 
-        //System.out.println("sort all employees with age in ascending order");
-        getAllEmployees().sort(Comparator.comparing(Employee::getAge));
 
-        //System.out.println("sort all employees with age in descending order");
-        getAllEmployees().sort(Comparator.comparing(Employee::getAge).reversed());
+        //System.out.println("sort all employees with age in ascending order and then print their ages and names");
+
+
+        //System.out.println("sort all employees with company id in ascending order and then print company id and names");
+
+
+        //System.out.println("sort all employees with age in descending order and then print their ages and names");
+
 
         //System.out.println("*** sort all employees with full name in ascending order and print only their full names ***");
-//        getAllEmployees().sort(Comparator.comparing(Employee::getFullName));
-//        getAllEmployees().forEach(employee -> System.out.println(employee.getFullName()));
+
 
         //System.out.println("*** sort all employees with full name in descending order and print only their full names ***");
-        //   getAllEmployees().sort(Comparator.comparing(employee -> employee.getFullName(), Comparator.reverseOrder()));  // works
-//        getAllEmployees().sort(Comparator.comparing(Employee::getFullName, Comparator.reverseOrder())); // works
-//     //   getAllEmployees().sort(Comparator.comparing(employee -> employee.getFullName()).reversed());  compile time error
-//        getAllEmployees().sort(Comparator.comparing(Employee::getFullName).reversed()); // works
-//        getAllEmployees().forEach(employee -> System.out.println(employee.getFullName()));
+
 
         //System.out.println("***sort all employees with company name in ascending order***");
-        getAllEmployees().sort(Comparator.comparing(employee -> employee.getCompany().getCompanyName()));
-        Collections.sort(getAllEmployees(), Comparator.comparing(employee -> employee.getCompany().getCompanyName()));
 
 
         //System.out.println("***sort all employees with company name in descending order***");
-        getAllEmployees().sort(Comparator.comparing(employee -> employee.getCompany().getCompanyName(), Comparator.reverseOrder()));
 
 
-        //System.out.println("sort all employees with company id in ascending order");
+        //System.out.println("***sort all employees with their gender in ascending order and then with their company name in descending order***");
 
     }
 }
